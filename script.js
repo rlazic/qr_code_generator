ffe5378411b6e67cf1547570fde9e6fec5363d72
rlazic <118766710+rlazic@users.noreply.github.com>
2023-10-13 12:12:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const input = document.getElementById('qr-input');
-    const generateButton = document.getElementById('generate-button');
+    const generateButton = document.getElementById('generate');
     const qrcodeContainer = document.getElementById('qrcode');
 
     generateButton.addEventListener('click', function () {
